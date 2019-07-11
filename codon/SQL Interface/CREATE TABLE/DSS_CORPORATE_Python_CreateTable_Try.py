72a0e4f0284e50ebf567370b99ae0b64f23556b7
Murdo Moyse <D-MUMO2@Dev.Green.Net>
2019-07-11 11:16:01 +0100

    Abstract sql query

--- codon/SQL Interface/CREATE TABLE/DSS_CORPORATE_Python_CreateTable_Try.py
+++ codon/SQL Interface/CREATE TABLE/DSS_CORPORATE_Python_CreateTable_Try.py
@@ -1,24 +1,30 @@
 import pyodbc
 
 # pyodbc connection
-cnxn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=GLDMSRV009;'
-                      'Database=DSS_CORPORATE;'
-                      'Trusted_Connection=yes;')
+cnxn = pyodbc.connect("""Driver={SQL Server};
+                      Server=GLDMSRV009;
+                      Database=DSS_CORPORATE;
+                      Trusted_Connection=yes;""")
+                    #   UID=USER;
+                    #   PWD=PASSWORD)
 
-try:
-    # Edit the SQL query to align to your table, schema and field constraints
-    cursor = cnxn.cursor()
-    cursor.execute('''CREATE TABLE [dbo].[AD_Users2](
+# SQL query to execute
+sql = """
+CREATE TABLE [dbo].[AD_Users2](
     [Name] [nvarchar](50) NOT NULL,
     [Pre_Windows_2000_Logon_Name] [nvarchar](50) NOT NULL,
     [E_Mail_Address] [nvarchar](50) NOT NULL
     )
-    ''')
+"""
+
+try:
+    # Execute sql
+    cursor = cnxn.cursor()
+    cursor.execute(sql)
 except TypeError as error:
     print(error)
 
-# Commit the transaction to 'save' the changes
+# Commit the transaction to 'save' the changes, then close the connection
 cnxn.commit()
 cnxn.close()
 
