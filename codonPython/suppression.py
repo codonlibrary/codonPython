3072ce752c1c0d78f9f88f5974b4cb7206647f55
Murdo Moyse <D-MUMO2@Dev.Green.Net>
2019-07-31 15:55:56 +0100

    Merge into local

--- codonPython/suppression.py
+++ codonPython/suppression.py
@@ -1,1 +1,36 @@
-import math
+def suppress_value(valuein: int, rc:str = '*')->str:
+    """
+    Suppress values less than or equal to 7
+
+    This function takes the metric value and suppresses it if it is less than or equal to 7. 
+    If the metric value is 0 then it will remain as 0.
+
+    Parameters
+    ----------
+    valuein : int
+        Metric value
+    rc : str
+        Replacement character if value needs suppressing
+
+    Returns
+    -------
+    out : str
+        Suppressed value (*), 0 or valuein if greater than 7
+
+    Examples
+    --------
+    >>> suppress_value(3)
+    '*'
+    >>> suppress_value(24)
+    '24'
+    >>> suppress_value(0)
+    '0'
+    """
+
+    if valuein ==0:
+        valueout = str(valuein)
+    if 1 <= valuein <= 7:
+        valueout = rc
+    if valuein >7:
+        valueout = str(valuein)
+    return valueout
