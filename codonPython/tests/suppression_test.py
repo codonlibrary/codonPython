76258643906dc6a6e557fe511f2208255894834c
Murdo Moyse <D-MUMO2@Dev.Green.Net>
2019-08-05 09:16:43 +0100

    autopep8

--- codonPython/tests/suppression_test.py
+++ codonPython/tests/suppression_test.py
@@ -1,23 +1,25 @@
 from codonPython.suppression import suppress_value
 import pytest
 
+
 @pytest.mark.parametrize("to_suppress, expected", [
     (0, 0),
     (2, "*"),
     (5, "*"),
     (8, 10),
-    (14, 15),
+    (16, 15),
     (57, 60),
     (10023, 10025)
 ])
 def test_suppress_value_BAU(to_suppress, expected):
     assert expected == suppress_value(to_suppress)
 
+
 @pytest.mark.parametrize("to_suppress", [
     -1,
     4.2,
     100000001
 ])
-def test_suppress_value_valueErrors(to_suppress):  
-    with pytest.raises(ValueError): 
+def test_suppress_value_valueErrors(to_suppress):
+    with pytest.raises(ValueError):
         suppress_value(to_suppress)
