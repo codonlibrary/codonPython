a9df5b12e2c2cc0f40ddaa1759caaaa7fcd9feef
Murdo Moyse <D-MUMO2@Dev.Green.Net>
2019-09-04 09:27:54 +0100

    Variable name change to t

--- codonPython/tolerance.py
+++ codonPython/tolerance.py
@@ -6,16 +6,16 @@ import statsmodels.api as sm
 from statsmodels.sandbox.regression.predstd import wls_prediction_std
 
 
-def check_tolerance(X, y, to_exclude: int = 1, poly_features: int = 2, alpha: float = 0.05) -> pd.DataFrame:
+def check_tolerance(t, y, to_exclude: int = 1, poly_features: int = 2, alpha: float = 0.05) -> pd.DataFrame:
     """
     Check that some future values are within a weighted least squares confidence interval.
 
     Parameters
     ----------
-    X : np.array
-        N independent variables of shape (N, 1).
+    t : np.array
+        N explanatory time bins of shape (N, 1).
     y : np.array
-        The corresponding dependent variable values to X, of shape (N, 1).
+        The corresponding response variable values to X, of shape (N, 1).
     to_exclude : int, default = 1
         How many of the last y values will have their tolerances checked.
     poly_features : int, default = 2
@@ -37,7 +37,7 @@ def check_tolerance(X, y, to_exclude: int = 1, poly_features: int = 2, alpha: fl
     Examples
     --------
     >>> check_tolerance(
-    ...     X = np.array([1001,1002,1003,1004,1005,1006]),
+    ...     t = np.array([1001,1002,1003,1004,1005,1006]),
     ...     y = np.array([2,3,4,4.5,5,5.1]),
     ... ).round(3).to_dict()
     {'yhat_u': {0: 6.061}, 'yobs': {0: 5.1}, 'yhat': {0: 5.2}, 'yhat_l': {0: 4.339}}
@@ -48,7 +48,7 @@ def check_tolerance(X, y, to_exclude: int = 1, poly_features: int = 2, alpha: fl
     if not isinstance(alpha, float) or 0 >= alpha >= 1:
         raise ValueError("Please input a float between 0 and 1 for alpha.")
 
-    N = len(X)
+    N = len(t)
 
     if not isinstance(to_exclude, int) or N <= to_exclude < 1:
         raise ValueError("Please input an integer between 1 and your sample size to exclude.")
@@ -56,8 +56,8 @@ def check_tolerance(X, y, to_exclude: int = 1, poly_features: int = 2, alpha: fl
         raise ValueError("Your sample size is smaller than 4. This will not produce a good model.")
 
     # Sort data by X increasing
-    idx = np.argsort(X)
-    X = X[idx]
+    idx = np.argsort(t)
+    t = t[idx]
     y = y[idx]
 
     transforms = make_pipeline(
@@ -66,19 +66,19 @@ def check_tolerance(X, y, to_exclude: int = 1, poly_features: int = 2, alpha: fl
     )
 
     # Fit transforms to train data, apply them to all data
-    fitted_transforms = transforms.fit(X[:-to_exclude].reshape(-1, 1))
-    X = fitted_transforms.transform(X.reshape(-1, 1))
+    fitted_transforms = transforms.fit(t[:-to_exclude].reshape(-1, 1))
+    t = fitted_transforms.transform(t.reshape(-1, 1))
 
-    X_train, y_train = X[:-to_exclude, :], y[:-to_exclude]
-    X_predict, y_predict = X[-to_exclude:, :], y[-to_exclude:]
+    t_train, y_train = t[:-to_exclude, :], y[:-to_exclude]
+    t_predict, y_predict = t[-to_exclude:, :], y[-to_exclude:]
 
     # Fit ordinary least squares model to the training data, then predict for the 
     # prediction data.
-    model = sm.OLS(y_train, X_train).fit()
-    yhat = model.predict(X_predict)
+    model = sm.OLS(y_train, t_train).fit()
+    yhat = model.predict(t_predict)
 
     # Calculate confidence interval of fitted model.
-    _, yhat_l, yhat_u = wls_prediction_std(model, X_predict, alpha=alpha)
+    _, yhat_l, yhat_u = wls_prediction_std(model, t_predict, alpha=alpha)
 
     return pd.DataFrame({
         "yhat_u" : yhat_u,
