3c9cb1ba22ed19e1bce3ad98278418f1b4b27faa
Murdo Moyse <D-MUMO2@Dev.Green.Net>
2019-09-04 12:42:47 +0100

    Multiple polynomial support and nan checking added

--- codonPython/tolerance.py
+++ codonPython/tolerance.py
@@ -6,7 +6,7 @@ import statsmodels.api as sm
 from statsmodels.sandbox.regression.predstd import wls_prediction_std
 
 
-def check_tolerance(t, y, to_exclude: int = 1, poly_features: int = 2, alpha: float = 0.05) -> pd.DataFrame:
+def check_tolerance(t, y, to_exclude: int = 1, poly_features: list = [1, 2], alpha: float = 0.05) -> pd.DataFrame:
     """
     Check that some future values are within a weighted least squares confidence interval.
 
@@ -18,8 +18,10 @@ def check_tolerance(t, y, to_exclude: int = 1, poly_features: int = 2, alpha: fl
         The corresponding response variable values to X, of shape (N, 1).
     to_exclude : int, default = 1
         How many of the last y values will have their tolerances checked.
-    poly_features : int, default = 2
-        Degree of polynomial features to fit to the data.
+    poly_features : list, default = [1, 2]
+        List of degrees of polynomial features to fit to the data. One model will be
+        produced for each number in the list, eg. the default will fit a linear and
+        a second degree polynomial to the data and return both sets of results.
     alpha : float, default = 0.05
         Alpha parameter for the weighted least squares confidence interval.
 
@@ -28,10 +30,11 @@ def check_tolerance(t, y, to_exclude: int = 1, poly_features: int = 2, alpha: fl
     -------
     pd.DataFrame
         DataFrame of shape (to_exclude, 4) containing:
-            "yhat_u" : Upper condfidence interval for y
-            "yobs"   : Observed value for y
-            "yhat"   : Predicted value for y
-            "yhat_l" : Lower confidence interval for y
+            "yhat_u"    : Upper condfidence interval for y
+            "yobs"      : Observed value for y
+            "yhat"      : Predicted value for y
+            "yhat_l"    : Lower confidence interval for y
+            "polynomial": Degree of polynomial fit to data
 
 
     Examples
@@ -39,50 +42,69 @@ def check_tolerance(t, y, to_exclude: int = 1, poly_features: int = 2, alpha: fl
     >>> check_tolerance(
     ...     t = np.array([1001,1002,1003,1004,1005,1006]),
     ...     y = np.array([2,3,4,4.5,5,5.1]),
-    ... ).round(3).to_dict()
-    {'yhat_u': {0: 6.061}, 'yobs': {0: 5.1}, 'yhat': {0: 5.2}, 'yhat_l': {0: 4.339}}
+    ...     to_exclude = 2,
+    ...     poly_features = [2],
+    ... )
+          yhat_u  yobs   yhat    yhat_l  polynomial
+    0   9.077182   5.0  4.875  0.672818           2
+    1  13.252339   5.1  4.975 -3.302339           2
     """
 
-    if not isinstance(poly_features, int) or 0 >= poly_features >= 4:
-        raise ValueError("Please input an integer from 0 to 4 for poly_features.")
+    if not isinstance(poly_features, list):
+        raise ValueError("Please input a list of integers from 0 to 4 for poly_features.")
+    assert all([0 <= degree <= 4 for degree in poly_features]), (
+      "Please ensure all numbers in poly_features are from 0 to 4."  
+    )
     if not isinstance(alpha, float) or 0 >= alpha >= 1:
         raise ValueError("Please input a float between 0 and 1 for alpha.")
+    if not isinstance(to_exclude, int) or len(t) <= to_exclude < 1:
+        raise ValueError("Please input an integer between 1 and your sample size for to_exclude.")
+    assert ((len(t) - to_exclude) >= 4), (
+        """The sample size for your model is smaller than 4. This will not produce a good 
+        model. Either reduce to_exclude or increase your sample size to continue."""
+    )
+    assert np.isfinite(y).all(), (
+        "Your sample contains missing or infinite values for y. Please exclude these values to continue."
+    )
 
-    N = len(t)
-
-    if not isinstance(to_exclude, int) or N <= to_exclude < 1:
-        raise ValueError("Please input an integer between 1 and your sample size to exclude.")
-    if N < 4:
-        raise ValueError("Your sample size is smaller than 4. This will not produce a good model.")
 
     # Sort data by X increasing
     idx = np.argsort(t)
     t = t[idx]
     y = y[idx]
+    results = pd.DataFrame()
 
-    transforms = make_pipeline(
-        StandardScaler(),
-        PolynomialFeatures(degree=poly_features),
-    )
+    for degree in poly_features:
+        transforms = make_pipeline(
+            StandardScaler(),
+            PolynomialFeatures(degree=degree),
+        )
 
-    # Fit transforms to train data, apply them to all data
-    fitted_transforms = transforms.fit(t[:-to_exclude].reshape(-1, 1))
-    t = fitted_transforms.transform(t.reshape(-1, 1))
+        # Fit transforms to training data, apply to all data.
+        fitted_transforms = transforms.fit(t[:-to_exclude].reshape(-1, 1))
+        _t = fitted_transforms.transform(t.reshape(-1, 1))
 
-    t_train, y_train = t[:-to_exclude, :], y[:-to_exclude]
-    t_predict, y_predict = t[-to_exclude:, :], y[-to_exclude:]
+        t_train, y_train = _t[:-to_exclude, :], y[:-to_exclude]
+        t_predict, y_predict = _t[-to_exclude:, :], y[-to_exclude:]
 
-    # Fit ordinary least squares model to the training data, then predict for the 
-    # prediction data.
-    model = sm.OLS(y_train, t_train).fit()
-    yhat = model.predict(t_predict)
+        # Fit ordinary least squares model to the training data, then predict for the
+        # prediction data.
+        model = sm.OLS(y_train, t_train).fit()
+        yhat = model.predict(t_predict)
 
-    # Calculate confidence interval of fitted model.
-    _, yhat_l, yhat_u = wls_prediction_std(model, t_predict, alpha=alpha)
+        # Calculate confidence interval of fitted model.
+        _, yhat_l, yhat_u = wls_prediction_std(model, t_predict, alpha=alpha)
 
-    return pd.DataFrame({
-        "yhat_u" : yhat_u,
-        "yobs" : y_predict,
-        "yhat" : yhat,
-        "yhat_l" : yhat_l,
-    })
+        # Store model results in master frame
+        results = results.append(
+            pd.DataFrame({
+              "yhat_u" : yhat_u,
+              "yobs" : y_predict,
+              "yhat" : yhat,
+              "yhat_l" : yhat_l,  
+              "polynomial" : degree
+            })
+        )
+
+    return results
+    
