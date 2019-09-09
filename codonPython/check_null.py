5e485c3402f342d274ade941c8fd15b1e36c2439
George Robertson <GERO1@hscic.gov.uk>
2019-09-09 14:57:35 +0100

    Checks for value and key errors. Also, columns_to_be_checked changed to only accept tuple.

--- codonPython/check_null.py
+++ codonPython/check_null.py
@@ -1,7 +1,7 @@
 import numpy
 import pandas as pd
 
-def check_null(dataframe: pd.DataFrame, columns_to_be_checked: list) -> bool:
+def check_null(dataframe: pd.DataFrame, columns_to_be_checked: tuple) -> bool:
     """
     Checks a pandas dataframe for null values
 
@@ -11,7 +11,7 @@ def check_null(dataframe: pd.DataFrame, columns_to_be_checked: list) -> bool:
     ----------
     data : pandas.DataFrame
         Dataframe to read
-    columns_to_be_checked: list
+    columns_to_be_checked: tuple
         Given dataframe columns to be checked for null values
 
     Returns
@@ -21,11 +21,17 @@ def check_null(dataframe: pd.DataFrame, columns_to_be_checked: list) -> bool:
 
     Examples
     --------
-    >>> check_null(dataframe = pd.DataFrame({'col1': [1,2], 'col2': [3,4]}), columns_to_be_checked = ['col1'])
+    >>> check_null(dataframe = pd.DataFrame({'col1': [1,2], 'col2': [3,4]}), columns_to_be_checked = ('col1'))
     0
-    >>> check_null(dataframe = pd.DataFrame({'col1': [1,numpy.nan], 'col2': [3,4]}), columns_to_be_checked = ['col1'])
+    >>> check_null(dataframe = pd.DataFrame({'col1': [1,numpy.nan], 'col2': [3,4]}), columns_to_be_checked = ('col1'))
     1
     """
+
+    for eachVal in columns_to_be_checked:
+        if type(eachVal) != str:
+            raise ValueError("Please input string values for column names.")
+    if columns_to_be_checked not in dataframe.columns:
+        raise KeyError("Please check the column names correspond to values in the DataFrame.")
 
     null_count = 0
     for eachColumn in columns_to_be_checked:
