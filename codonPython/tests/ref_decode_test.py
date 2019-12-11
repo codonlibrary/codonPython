3b7d7f394d0bec58468008b13286685fbd2f3174
Sam Hollings <31413973+Naawww@users.noreply.github.com>
2019-12-11 19:37:03 +0000

    Update ref_decode_test.py
    made first test: test_ref_decode_col_num

--- codonPython/tests/ref_decode_test.py
+++ codonPython/tests/ref_decode_test.py
@@ -4,11 +4,50 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from SQL_connections import conn_DSS, conn_DSS2016UAT
+import sqlite3
+import pytest
 
-test_df = pd.DataFrame([{'ORG_CODE': 'X24','ORG_CODE_answer': 'NHS ENGLAND','SNOMED_CODE': '74964007', 'SNOMED_CODE_answer' : 'Other (qualifier value)', 'ICD10_CODE': 'A00','ICD10_CODE_answer': 'Cholera'},
-                           {'ORG_CODE': 'Q71','ORG_CODE_answer': 'NHS ENGLAND LONDON', 'SNOMED_CODE': '260413007', 'SNOMED_CODE_answer' : 'None', 'ICD10_CODE': 'A010','ICD10_CODE_answer': 'Typhoid fever'},
-                           {'ORG_CODE': 'X26','ORG_CODE_answer': 'NHS DIGITAL', 'SNOMED_CODE': '276727009', 'SNOMED_CODE_answer' : 'Null', 'ICD10_CODE': 'A20','ICD10_CODE_answer': 'Plague'}])
+test_df = pd.DataFrame([{'OrganisationCode': 'X24','Name': 'NHS ENGLAND',
+                         'SNOMED_CODE': '74964007', 'SNOMED_CODE_answer' : 'Other (qualifier value)', 
+                         'ICD10_CODE': 'A00','ICD10_CODE_answer': 'Cholera'},
+                           {'OrganisationCode': 'Q71','Name': 'NHS ENGLAND LONDON', 
+                            'SNOMED_CODE': '260413007', 'SNOMED_CODE_answer' : 'None', 
+                            'ICD10_CODE': 'A010','ICD10_CODE_answer': 'Typhoid fever'},
+                           {'OrganisationCode': 'X26','Name': 'NHS DIGITAL', 
+                            'SNOMED_CODE': '276727009', 'SNOMED_CODE_answer' : 'Null', 
+                            'ICD10_CODE': 'A20','ICD10_CODE_answer': 'Plague'}])
 
+conn = sqlite3.connect(':memory:')
+
+ref_decode(df, org_cols=[], sno_cols=[], icd10_cols=[])
+
+test_df_1 = pd.concat([test_df, 
+                   test_df.rename(columns=dict(OrganisationCode='CCG'))[['CCG']],
+                   test_df.rename(columns=dict(SNOMED_CODE='snomed'))[['snomed']],
+                   test_df.rename(columns=dict(OrganisationCode='icd10'))[['icd10']],    
+                  ],axis=1)
+test_df_1.to_sql(test,conn)
+
+@pytest.mark.parametrize("org_cols_test, sno_cols_test, icd10_cols_test, num_cols",
+                         [('OrganisationCode', None, None, 1),
+                          ('OrganisationCode', 'snomed', None, 2),
+                          ('OrganisationCode', 'snomed', 'icd10', 3),
+                          (None, 'snomed', 'icd10', 2),
+                          (None, 'snomed', None, 1),
+                          ('OrganisationCode', 'snomed', None, 2),
+                          ('OrganisationCode', None, 'icd10', 2),
+                          (None, None, 'icd10', 1),
+                         ])
+def test_ref_decode_col_num():
+    df_out = ref_decode(test_df_1, 
+                        org_cols=org_cols_test,
+                        sno_cols=sno_cols_test,
+                        icd10_cols=icd10_cols_test)
+    assert len(df_out.columns) == len(test_df_1) + num_cols
+    
+                        
+    
+    
 
 class Test_sno_col(unittest.TestCase):
     # Test the "sno_col()" function
