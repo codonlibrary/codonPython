0b5b237d7ed855d54c9673c973e3831d2ce86cb1
Sam Hollings <31413973+Naawww@users.noreply.github.com>
2019-12-11 17:32:44 +0000

    Update test_SQL_connections.py
    Pytest script for SQL_connections

--- codonPython/tests/test_SQL_connections.py
+++ codonPython/tests/test_SQL_connections.py
@@ -1,23 +1,15 @@
 '''test script for SQL_connections
 - test the connections can run a dummy script (SELECT 1 as [Code], 'test' as [Name])'''
 import pandas as pd
-import unittest
-import common_python.SQL_connections as conn 
+import pytest
+import SQL_connections as conn 
 
-class Test_connections(unittest.TestCase):
-	# Test the SQL database connections
-	def setUp(self):
-		self.conn = {'MH': conn.conn_MH()}
-		self.conn['HES'] = conn.conn_HES()
-		self.conn['DSS'] = conn.conn_DSS()
-		self.conn['DSS2016UAT'] = conn.conn_DSS2016UAT()
-
-	def test_SELECT1(self):
-		for conn_name, conn in self.conn.items():
-			# Check connection can run simple SQL script
-			with self.subTest(conn=conn_name):
-				self.assertEqual(pd.read_sql("""SELECT 1 as [Code], 'Test' as [Name]""", conn).iloc[0,0],
-								 pd.DataFrame([{'Code': 1, 'Name' : 'Test'}]).iloc[0,0])
-
-if __name__ == '__main__':
-	unittest.main()
+@pytest.mark.parametrize("connection", 
+			 [conn.conn_DSS(),
+			  conn.conn_DSS2016UAT
+			 ])
+def test_SELECT1(connection):
+	sql = """SELECT 1 as [Code], 'Test' as [Name]"""
+	result = pd.read_sql("""SELECT 1 as [Code], 'Test' as [Name]""", connection).iloc[0,0]
+	expected = pd.DataFrame([{'Code': 1, 'Name' : 'Test'}]).iloc[0,0]
+	assert result == expected
