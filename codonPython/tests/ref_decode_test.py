851ff68e6518958542fee4528341bd9932fc8ca2
Sam Hollings <31413973+Naawww@users.noreply.github.com>
2019-12-11 20:32:23 +0000

    Update ref_decode_test.py
    Cleaned up the unit test.

--- codonPython/tests/ref_decode_test.py
+++ codonPython/tests/ref_decode_test.py
@@ -18,9 +18,6 @@ test_df = pd.DataFrame([{'OrganisationCode': 'X24','Name': 'NHS ENGLAND',
                             'ICD10_CODE': 'A20','ICD10_CODE_answer': 'Plague'}])
 
 conn = sqlite3.connect(':memory:')
-
-ref_decode(df, org_cols=[], sno_cols=[], icd10_cols=[])
-
 test_df_1 = pd.concat([test_df, 
                    test_df.rename(columns=dict(OrganisationCode='CCG'))[['CCG']],
                    test_df.rename(columns=dict(SNOMED_CODE='snomed'))[['snomed']],
@@ -42,29 +39,6 @@ def test_ref_decode_col_num():
     df_out = ref_decode(test_df_1, 
                         org_cols=org_cols_test,
                         sno_cols=sno_cols_test,
-                        icd10_cols=icd10_cols_test)
+                        icd10_cols=icd10_cols_test,
+                        conn = conn)
     assert len(df_out.columns) == len(test_df_1) + num_cols
-    
-                        
-    
-    
-
-class Test_sno_col(unittest.TestCase):
-    # Test the "sno_col()" function
-    def setUp(self):
-
-        sno_df = cf.sno_col(test_df,'SNOMED_CODE')
-        self.SNOMED_desc = sno_df['SNOMED_CODE_desc']
-
-    def test_desc_col(self):
-        self.assertIn(self.SNOMED_desc.iloc[0],test_df['SNOMED_CODE_answer'].iloc[0])
-
-class Test_org_col(unittest.TestCase):
-    # Test the "sno_col()" function
-    def setUp(self):
-
-        df = cf.org_col(test_df,'ORG_CODE')
-        self.ORG_desc = df['ORG_CODE_name']
-
-    def test_desc_col(self):
-        self.assertIn(self.ORG_desc.iloc[0],test_df['ORG_CODE_answer'].iloc[0])
