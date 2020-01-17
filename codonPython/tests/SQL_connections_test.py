6f79f331e5e37e457cab4069b0014a1e10d01c1b
SAHO9 <sam.hollings1@nhs.net>
2020-01-17 09:36:30 +0000

    Addressed comments https://github.com/codonlibrary/codonPython/pull/113/files/accda9cce9f2dfe6b6433d1fe090ad40f7140bae - removed unused import from codonPython.SQL_connections - removed unused "sql" variable and definition

--- codonPython/tests/SQL_connections_test.py
+++ codonPython/tests/SQL_connections_test.py
@@ -1,6 +1,5 @@
 '''test script for SQL_connections
 - test the connections can run a dummy script (SELECT 1 as [Code], 'test' as [Name])'''
-from codonPython import SQL_connections
 import pandas as pd
 import pytest
 import codonPython.SQL_connections as conn
@@ -10,7 +9,6 @@ import codonPython.SQL_connections as conn
 			  conn.conn_DSS2016UAT()
 			 ])
 def test_SELECT1(connection):
-	sql = """SELECT 1 as [Code], 'Test' as [Name]"""
 	result = pd.read_sql("""SELECT 1 as [Code], 'Test' as [Name]""", connection).iloc[0,0]
 	expected = pd.DataFrame([{'Code': 1, 'Name' : 'Test'}]).iloc[0,0]
 	assert result == expected
