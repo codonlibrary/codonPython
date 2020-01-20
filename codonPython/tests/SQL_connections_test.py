3bf73f23a6a6385ce38472500def46860f581587
SAHO9 <sam.hollings1@nhs.net>
2020-01-20 10:57:49 +0000

    Addressed pep8 violations.

--- codonPython/tests/SQL_connections_test.py
+++ codonPython/tests/SQL_connections_test.py
@@ -2,14 +2,14 @@
 - test the connections can run a dummy script (SELECT 1 as [Code], 'test' as [Name])'''
 import pandas as pd
 import pytest
-import codonPython.SQL_connections as conn
+import codonPython.SQL_connections as Conn
 
 
-@pytest.mark.parametrize("connection", 
-                        [conn.conn_dummy(),
-                        conn.conn_dummy('test.db')
-                        ])
-def test_SELECT1(connection):
-    result = pd.read_sql("""SELECT 1 as [Code], 'Test' as [Name]""", connection).iloc[0,0]
-    expected = pd.DataFrame([{'Code': 1, 'Name' : 'Test'}]).iloc[0,0]
+@pytest.mark.parametrize("connection",
+                         [conn.conn_dummy(),
+                          conn.conn_dummy('test.db')
+                          ])
+def test_select1(connection):
+    result = pd.read_sql("""SELECT 1 as [Code], 'Test' as [Name]""", connection).iloc[0, 0]
+    expected = pd.DataFrame([{'Code': 1, 'Name': 'Test'}]).iloc[0, 0]
     assert result == expected
