c06ed14ea95449200205eeed4f0096b4f8f03366
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2020-01-24 14:50:32 +0000

    ODS tests

---
+++ codonPython/tests/ODS_test.py
@@ -0,0 +1,27 @@
+import pytest
+import numpy as np
+from codonPython.ODS_lookup import query_api, get_addresses
+
+
+def test_successful_query():
+    NHSD_code = "X26"
+    result = query_api(NHSD_code)
+    assert result["Organisation"]["Name"] == "NHS DIGITAL"
+
+
+def test_unsuccessful_query():
+    invalid_code = "ASDF"
+    with pytest.raises(ValueError):
+        query_api(invalid_code)
+
+
+def test_wrong_type():
+    invalid_code = 0
+    with pytest.raises(ValueError):
+        query_api(invalid_code)
+
+
+def test_unsuccessful_address_query():
+    invalid_code = ["ASDF", np.nan, None]
+    result = get_addresses(invalid_code)
+    assert result.empty
