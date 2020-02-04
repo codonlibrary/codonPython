0d6b32b06c0ce37c85a129730c8d65976f2de748
Murdo <49711704+murd0@users.noreply.github.com>
2020-02-04 09:44:58 +0000

    Update -v before switch to formal release schedule
    Versions from now on will be controlled.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='codonPython',
-    version='0.2.1',
+    version='0.2.2',
     license='BSD',
     packages=['codonPython', ],
     install_requires=requirements,
