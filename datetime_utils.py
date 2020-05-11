2dc714d2c77f31cefcc440656d244e2a36fdf426
RomanPenza <57672372+RomanPenza@users.noreply.github.com>
2020-05-11 08:22:53 +0100

    Add files via upload
    added datetime_utils module

---
+++ datetime_utils.py
@@ -0,0 +1,45 @@
+import datetime
+from dateutil.relativedelta import relativedelta
+
+def add_time(date : str, days : int = 0, months : int = 0, years : int = 0) -> str: 
+    
+    """
+    This function takes a date as a string input in the 'yyyy-mm-dd' format and returns 
+    transpose of the date, given the specified number of days, months and years. 
+    To substract days, months, years from the date passed, use negative values. 
+    
+    Parameters
+    -----------
+    date : string 
+        Original date
+        
+    days : int
+        Days to add/substract
+        default = 0
+        
+    months : int
+        Months to add/substract
+        default = 0
+        
+    years : int
+        Years to add/substract
+        default = 0
+        
+    Returns
+    -------
+    str
+    
+    Examples
+    -------
+    >>>add_time('2018-03-04', months = 4)
+    '2018-07-04'
+     
+    >>>add_time('2018-03-04', days = -90)
+    '2017-12-04'
+
+    >>>add_time('2018-03-04', years = 2)
+    '2020-03-04'
+
+    """
+    
+    return str((datetime.datetime.strptime(date,'%Y-%m-%d') + relativedelta(days = int(days), months = int(months), years = int(years))).strftime(('%Y-%m-%d')))
---
+++ datetime_utils_test.py
@@ -0,0 +1,34 @@
+from datetime_utils import add_time
+import pytest
+
+#------------------------------add_time----------------------------------------
+
+@pytest.mark.parametrize("date, years, months, days, expected", [
+    ('2019-01-01', 1, 2,3, '2020-03-04')])
+
+def test_add_time_all(date, years, months, days, expected):
+    assert add_time(date, years = years, months = months, days = days) == expected
+
+
+@pytest.mark.parametrize("date, years, expected", [
+    ('2019-01-01', -2, '2017-01-01')])
+
+def test_add_time_years(date, years, expected):
+    assert add_time(date, years = years) == expected
+
+
+@pytest.mark.parametrize("date, months, expected", [
+    ('2019-01-01', 5, '2019-06-01')])
+
+def test_add_time_months(date, months, expected):
+    assert add_time(date, months = months) == expected
+
+
+@pytest.mark.parametrize("date, days, expected", [
+    ('2019-01-01', 49, '2019-02-19')])
+
+def test_add_time_days(date, days, expected):
+    assert add_time(date, days = days) == expected
+
+
+
