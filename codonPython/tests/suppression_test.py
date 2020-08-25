546b7613854673a6258ad1c1c466dd44cae0800b
Natasha Chetwynd <natasha.chetwynd1@nhs.net>
2020-08-25 14:43:25 +0100

    CON-154 fix suppression function (codonlibrary#126)

--- codonPython/tests/suppression_test.py
+++ codonPython/tests/suppression_test.py
@@ -1,16 +1,16 @@
-from codonPython.suppression import suppress_value
+from codonPython.suppression import central_suppresion_method
 import pytest
 
 
 @pytest.mark.parametrize(
     "to_suppress, expected",
-    [(0, "0"), (2, "*"), (5, "*"), (8, "10"), (16, "15"), (57, "55"), (10023, "10025")],
+    [(0, "0"), (2, "5"), (5, "5"), (8, "10"), (16, "15"), (57, "55"), (10023, "10025")],
 )
-def test_suppress_value_BAU(to_suppress, expected):
-    assert expected == suppress_value(to_suppress)
+def test_central_suppresion_method_BAU(to_suppress, expected):
+    assert expected == central_suppresion_method(to_suppress)
 
 
-@pytest.mark.parametrize("to_suppress", [-1, 4.2, 100000001])
+@pytest.mark.parametrize("to_suppress", [-1, 4.2, 5000000001])
 def test_suppress_value_valueErrors(to_suppress):
     with pytest.raises(ValueError):
-        suppress_value(to_suppress)
+        central_suppresion_method(to_suppress)
