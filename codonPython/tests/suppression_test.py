5905f9f355e3517b5f4d3fdb5c3774ce21e3bc2b
Natasha Chetwynd <natasha.chetwynd1@nhs.net>
2020-09-08 13:42:50 +0100

    CON-154 fix suppression function (codonlibrary#126) - fix typo

--- codonPython/tests/suppression_test.py
+++ codonPython/tests/suppression_test.py
@@ -6,11 +6,11 @@ import pytest
     "to_suppress, expected",
     [(0, "0"), (2, "5"), (5, "5"), (8, "10"), (16, "15"), (57, "55"), (10023, "10025")],
 )
-def test_central_suppresion_method_BAU(to_suppress, expected):
-    assert expected == central_suppresion_method(to_suppress)
+def test_central_suppression_method_BAU(to_suppress, expected):
+    assert expected == central_suppression_method(to_suppress)
 
 
 @pytest.mark.parametrize("to_suppress", [-1, 4.2, 5000000001])
 def test_suppress_value_valueErrors(to_suppress):
     with pytest.raises(ValueError):
-        central_suppresion_method(to_suppress)
+        central_suppression_method(to_suppress)
